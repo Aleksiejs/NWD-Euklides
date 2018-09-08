89ca3855ecad11cb2c6a959cfd54639fe4fd1f7a
Aleksiejs <43091128+Aleksiejs@users.noreply.github.com>
2018-09-08 14:27:27 +0200

    Initial commit

---
+++ NWD_euklides.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+#przykładowe liczby to 1989 i 867 wynik to 51
+
+
+# In[ ]:
+
+
+a = int(input("Podaj pierwszą liczbę:\n"))
+b = int(input("Podaj drugą liczbę:\n"))
+
+while a > b:
+    a = a - b
+    while b > a:
+        b = b - a
+    
+print (f"Największy wspólny dzielnik liczb 1989 i 867 to {a}")
+
+
+# In[ ]:
+
+
+a = int(input("Podaj pierwszą liczbę:\n"))
+b = int(input("Podaj drugą liczbę:\n"))
+
+while b != 0:
+    c = a%b
+    #zastąp a liczbą b, następnie b liczbą c
+    a = b
+    b = c
+    #jeżeli wartość b wynosi 0, to a jest szukaną wartością NWD, w przeciwnym wypadku przejdź do kroku 1
+
+print (f"Największy wspólny dzielnik liczb 1989 i 867 to {a}")
+
